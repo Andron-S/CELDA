b5225c4335fa800c62028eb478edcfa048bd14a5
Andron-S <64315991+Andron-S@users.noreply.github.com>
2021-12-01 00:06:19 +0300

    Добавил АНИМАЦИЮ

--- app/src/main/java/com/app/celda/Activities/LaunchScreen.kt
+++ app/src/main/java/com/app/celda/Activities/LaunchScreen.kt
@@ -1,18 +1,27 @@
 package com.app.celda
 
 import android.content.Intent
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View
+import android.widget.ImageView
 
 class LaunchScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_screen)
+        val mCpuImageView: ImageView = findViewById<View>(R.id.logo) as ImageView
+        val drawable: Drawable = mCpuImageView.getDrawable()
+        if (drawable is Animatable) {
+            (drawable as Animatable).start()
+        }
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }, 5000)
+        }, 3400)
     }
 
 }
