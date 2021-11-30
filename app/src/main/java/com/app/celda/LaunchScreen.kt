2ae72c5d8b8f42e3c350de683064109f311eca00
Andron-S <64315991+Andron-S@users.noreply.github.com>
2021-11-30 19:16:49 +0300

    Добавил переход и иконку приложения

--- app/src/main/java/com/app/celda/LaunchScreen.kt
+++ app/src/main/java/com/app/celda/LaunchScreen.kt
@@ -1,11 +1,18 @@
 package com.app.celda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LaunchScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_screen)
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 5000)
     }
+
 }
