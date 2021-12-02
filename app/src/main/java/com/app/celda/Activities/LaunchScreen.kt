6e930dae84beab1bdb088d2f07bc1624f94c0767
Andron-S <64315991+Andron-S@users.noreply.github.com>
2021-12-02 13:17:36 +0300

    обновил АНИМАЦИЮ

--- app/src/main/java/com/app/celda/Activities/LaunchScreen.kt
+++ app/src/main/java/com/app/celda/Activities/LaunchScreen.kt
@@ -8,20 +8,23 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 
 class LaunchScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_launch_screen)
+        val progressBar : ProgressBar = findViewById(R.id.progressBar)
         val mCpuImageView: ImageView = findViewById<View>(R.id.logo) as ImageView
         val drawable: Drawable = mCpuImageView.getDrawable()
         if (drawable is Animatable) {
             (drawable as Animatable).start()
         }
+
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
-        }, 3400)
+        }, 3900)
     }
 
 }
